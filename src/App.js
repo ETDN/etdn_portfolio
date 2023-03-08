05d8e581721991c1e7c8924ed24f9a4c37e11059
etdnp <etdn.prod@gmail.com>
2023-03-08 22:17:37 +0100

    setup, css variable, home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "./fonts/ARAME-REGULAR.TTF";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <h2>Hi, I'am</h2>
+      <h1>Emilie Teodoro Do Nascimento</h1>
+      <h2>Front-End / Software Development & UI Enthusiast</h2>
     </div>
   );
 }
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 export default App;
